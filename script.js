5ef2e922c7ea555cf847b703c7c7d643dd377445
maggieumber <maggieumber@gmail.com>
2020-05-09 16:44:06 -0500

    updated text

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var myFont;
+var black, extralight;
 
 function preload() {
-  myFont = loadFont("LeagueSpartan-Black.otf");
+  black = loadFont("LeagueSpartan-Black.otf");
+  extralight = loadFont("LeagueSpartan-Extralight.otf");
 }
 
 function setup() {
   createCanvas(600, 1200);
+  // background color
+  background(51, 79, 255); // blue color
 }
 
 function draw() {
-  background(50);
-  fill(255);
   textAlign(LEFT);
-  textFont(myFont);
-  textSize(100);
-  text("52", 50, 200);
-  text("PROJECTS", 50, 400);
+  textSize(120); // keep type uniform size
+
+  // 52 text & color
+  fill(255, 51, 79); // red color
+  textFont(black);
+  text("52", 50, mouseY - 100);
+
+  // PROJECTS text & color
+  fill(79, 255, 51);
+  textFont(extralight);
+  text("PROJECTS", 50, mouseY - 300);
+}
+
+
+function mousePressed() {
+  background(51, 79, 255); // blue color
 }
